609acc7873b6303d51d6202ac82a019473707e62
shariful-alam-2024 <sharifullinkdin2024@gmail.com>
2025-02-28 22:54:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var a = -9;
+if (a >= 0) {
+     console.log("a is a positive number",a);
+     
+}
+else {
+     console.log("a is not a positive number",a);
+     
+}
+
+let number = [1, -1, 3, -5, 6, -7, 5, 4, -9, -20,0,-0,0.1,-0.5,5.7,-3.5];
+let result = separateNumber(number);
+function separateNumber(arr) {
+     let positives = arr.filter(num => num > 0);
+     let negatives = arr.filter(num => num < 0);
+     let zero = arr.filter(num => num === 0);
+     return {positives ,negatives,zero}
+     
+}
+console.log(result.positives);
+
+console.log(result.negatives);
+
+console.log(result.zero);
